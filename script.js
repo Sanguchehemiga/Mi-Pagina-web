8d4e909e53c39dc721fd9203135125b7794998ea
JuanRyb <130872556+JuanRyb@users.noreply.github.com>
2023-10-10 20:17:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
+const tamagotchi = {
+    hunger: 100,
+    happiness: 100,
+    energy: 100,
+};
+
+function updateStatus() {
+    document.getElementById('hunger').textContent = tamagotchi.hunger;
+    document.getElementById('happiness').textContent = tamagotchi.happiness;
+    document.getElementById('energy').textContent = tamagotchi.energy;
+}
+
+function feed() {
+    tamagotchi.hunger += 10;
+    updateStatus();
+}
+
+function play() {
+    tamagotchi.happiness += 10;
+    tamagotchi.energy -= 5;
+    updateStatus();
+}
+
+function sleep() {
+    tamagotchi.energy += 20;
+    tamagotchi.hunger -= 10;
+    updateStatus();
+}
+
+document.getElementById('feed').addEventListener('click', feed);
+document.getElementById('play').addEventListener('click', play);
+document.getElementById('sleep').addEventListener('click', sleep);
+
+updateStatus();
 
